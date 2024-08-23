d1ad8fc95c456782e204342c11e649b7f36224cb
Nadine Werner <nadine-werner@teleos-web.de>
2024-08-23 10:16:31 +0200

    more styling

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,38 @@
 #   end
 require 'faker'
 list = []
-list_pet = []
+
+pet_urls = {
+  dogs: [
+    "https://res.cloudinary.com/dwalfnpav/image/upload/v1724349861/Dog2_rsgl1a.jpg",
+    "https://res.cloudinary.com/dwalfnpav/image/upload/v1724349861/Dog4_d1fvkd.jpg",
+    "https://res.cloudinary.com/dwalfnpav/image/upload/v1724349860/Dog3_hrfthk.jpg",
+    "https://res.cloudinary.com/dwalfnpav/image/upload/v1724349859/Dog5_le4gux.jpg",
+    "https://res.cloudinary.com/dwalfnpav/image/upload/v1724349849/Dog1_nlrh2q.jpg"
+  ],
+  cats: [
+    "https://res.cloudinary.com/dwalfnpav/image/upload/v1724349875/Cat4_apdtz6.jpg",
+    "https://res.cloudinary.com/dwalfnpav/image/upload/v1724349874/Cat5_civjs3.jpg",
+    "https://res.cloudinary.com/dwalfnpav/image/upload/v1724349871/Cat6_ey2mf2.jpg",
+    "https://res.cloudinary.com/dwalfnpav/image/upload/v1724349870/Cat7_fakcjm.jpg",
+    "https://res.cloudinary.com/dwalfnpav/image/upload/v1724349875/Cat3_k461iw.jpg"
+  ],
+  birds: [
+    "https://res.cloudinary.com/dwalfnpav/image/upload/v1724349893/Bird3_etndzh.jpg",
+    "https://res.cloudinary.com/dwalfnpav/image/upload/v1724349893/Bird2_qvauic.webp",
+    "https://res.cloudinary.com/dwalfnpav/image/upload/v1724349893/Bird1_yj0zom.jpg",
+    "https://res.cloudinary.com/dwalfnpav/image/upload/v1724349892/Bird4_e6lawp.jpg",
+    "https://res.cloudinary.com/dwalfnpav/image/upload/v1724349892/Bird5_jxjm8t.jpg"
+  ],
+  rabbits: [
+    "https://res.cloudinary.com/dwalfnpav/image/upload/v1724349895/Rabbit1_l3kj86.webp",
+    "https://res.cloudinary.com/dwalfnpav/image/upload/v1724349893/Rabbit3_vev4tx.webp",
+    "https://res.cloudinary.com/dwalfnpav/image/upload/v1724349893/Rabbit2_fxaorv.jpg",
+    "https://res.cloudinary.com/dwalfnpav/image/upload/v1724349893/Rabbit5_gnaydz.jpg",
+    "https://res.cloudinary.com/dwalfnpav/image/upload/v1724349891/Rabbit4_r5btef.webp"
+  ]
+}
+
 
 Booking.destroy_all
 Pet.destroy_all
@@ -29,8 +60,8 @@ while list.size < 15
   )
   list << user
 end
-5.times do
-  Pet.create!(
+5.times do |index|
+  pet = Pet.new(
     name: Faker::Creature::Dog.name,
     kind: "dog",
     age: rand(0..8),
@@ -38,11 +69,15 @@ end
     start_date: Faker::Date.between(from: '2024-09-1', to: '2024-10-1'),
     end_date: Faker::Date.between(from: '2024-11-5', to: '2024-11-7'),
     user: list.sample,
-    picture_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTGrwqiibW9aYEokXO71t1fcMOpGY67MGzWqw&s"
   )
+
+  file2 = URI.open(pet_urls[:dogs][index])
+  pet.profile_picture.attach(io: file2, filename: 'student_one.png', content_type: 'image/png')
+
+  pet.save
 end
-5.times do
-  Pet.create!(
+5.times do |index|
+  pet = Pet.new(
     name: Faker::Creature::Dog.name,
     kind: "cat",
     age: rand(0..8),
@@ -50,11 +85,15 @@ end
     start_date: Faker::Date.between(from: '2024-09-1', to: '2024-10-1'),
     end_date: Faker::Date.between(from: '2024-11-5', to: '2024-11-7'),
     user: list.sample,
-    picture_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2IH4b3hvDu80YCgesuXzZuXQB7_OZ7_sffw&s"
   )
+
+  file3 = URI.open(pet_urls[:cats][index])
+  pet.profile_picture.attach(io: file3, filename: 'student_one.png', content_type: 'image/png')
+
+  pet.save
 end
-5.times do
-  Pet.create!(
+5.times do |index|
+  pet = Pet.new(
     name: Faker::Creature::Dog.name,
     kind: "bird",
     age: rand(0..8),
@@ -62,11 +101,15 @@ end
     start_date: Faker::Date.between(from: '2024-09-1', to: '2024-10-1'),
     end_date: Faker::Date.between(from: '2024-11-5', to: '2024-11-7'),
     user: list.sample,
-    picture_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2IH4b3hvDu80YCgesuXzZuXQB7_OZ7_sffw&s"
   )
+
+  file4 = URI.open(pet_urls[:birds][index])
+  pet.profile_picture.attach(io: file4, filename: 'student_one.png', content_type: 'image/png')
+
+  pet.save
 end
-5.times do
-  Pet.create!(
+5.times do |index|
+  pet = Pet.new(
     name: Faker::Creature::Dog.name,
     kind: "rabbit",
     age: rand(0..8),
@@ -74,8 +117,10 @@ end
     start_date: Faker::Date.between(from: '2024-09-1', to: '2024-10-1'),
     end_date: Faker::Date.between(from: '2024-11-5', to: '2024-11-7'),
     user: list.sample,
-    picture_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2IH4b3hvDu80YCgesuXzZuXQB7_OZ7_sffw&s"
   )
+
+  file5 = URI.open(pet_urls[:rabbits][index])
+  pet.profile_picture.attach(io: file5, filename: 'student_one.png', content_type: 'image/png')
+
+  pet.save
 end
-
-
